19c13912be2c82dfc9f3fdeb6c0e3c77e9a8e2fb
WolvesFans <65891445+WolvesFans@users.noreply.github.com>
2025-05-01 16:23:56 +0700

    Update weather_service.dart

--- lib/utils/http/weather_service.dart
+++ lib/utils/http/weather_service.dart
@@ -6,7 +6,7 @@ import 'package:learn_weather_api/models/weather_model.dart';
 class WeatherService {
   //base url & api key
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-  static const String apiKey = '02c02b48840aae3c1811d6a07c550743';
+  static const String apiKey = '';
 
   //get data from url + api key based on city
   Future<WeatherModel> getWeather(String city) async {
